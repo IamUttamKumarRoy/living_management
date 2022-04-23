87d1b24bdcd31b5099c2acb13df4baba8294d518
Uttam Kumar Roy <cseuttamiu@gmail.com>
2022-04-23 23:55:11 +0600

    Add files via upload

---
+++ storage/framework/views/bc354def5d5dedbe1f5390be5ab306d005586d2b.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>Laravel 9 CRUD Application - ItSolutionStuff.com</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    
+
+<div class="container">
+
+    <?php echo $__env->yieldContent('content'); ?>
+
+</div>
+
+    
+
+</body>
+
+</html><?php /**PATH D:\workspace\xampp_814\htdocs\laravel_projects\living_management\resources\views/products/layout.blade.php ENDPATH**/ ?>
